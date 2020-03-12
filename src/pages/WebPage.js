a780dc6b055046e90894014b7076a67eb65ee641
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-03-12 17:42:29 +0000

    ColorPicker component and challenge complete

--- src/pages/WebPage.js
+++ src/pages/WebPage.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// import { Container } from './styles';
 import Name from "./../components/Name";
-import Page from "./../components/Page";
+import ColorPicker from "./../components/ColorPicker";
+
+import randomColor from "randomcolor";
+
 export default function WebPages() {
+  const [colors, setColors] = useState([]);
+  const [activeColor, setActiveColor] = useState("");
+
+  useEffect(() => {
+    const baseColor = randomColor().slice(1);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+      .then(res => res.json())
+      .then(res => {
+        setColors(res.colors.map(color => color.hex.value));
+        setActiveColor(res.colors[0].hex.value);
+      });
+  }, []);
+
   return (
-    <div>
-      <Name />
-      <Page />
-    </div>
+    <header style={{ borderTop: `10px solid ${activeColor}` }}>
+      <div className="app">
+        <Name />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        <ColorPicker
+          colors={colors}
+          activeColor={activeColor}
+          setActiveColor={setActiveColor}
+        />
+      </div>
+    </header>
   );
 }
